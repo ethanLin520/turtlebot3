152bdb642aad6d97e8d22eb9cd79ad089ab2d903
Ethan Lin <eeeeethanl@gmail.com>
2024-09-29 12:53:34 +0000

    clean repo

--- wall_follower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/comp3431/colcon_ws/src/wall_follower
-# Build directory: /home/comp3431/colcon_ws/src/wall_follower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
